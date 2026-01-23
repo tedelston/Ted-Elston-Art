c107c18f1b286f4532e7f5a07c0009e8534e81a1
tedelston <tedelston@yahoo.ie>
2026-01-23 22:10:19 +0000

    Update payment-methods.js

--- netlify/functions/payment-methods.js
+++ netlify/functions/payment-methods.js
@@ -6,10 +6,10 @@ exports.handler = async function(event, context) {
       "Access-Control-Allow-Origin": "*", 
     },
     body: JSON.stringify([{
-      id: "custom_stripe_checkout_final",
-      name: "FINAL TEST (Click Me)",
-      // FIX: Use a relative path to stop domain redirects
-      checkoutUrl: "/.netlify/functions/create-session", 
+      id: "custom_stripe_checkout_absolute",
+      name: "FINAL TEST (Absolute URL)",
+      // FIX: Use the full Netlify address to ensure Snipcart trusts it
+      checkoutUrl: "https://tedelston.netlify.app/.netlify/functions/create-session", 
       iconUrl: "https://upload.wikimedia.org/wikipedia/commons/b/ba/Stripe_Logo%2C_revised_2016.svg"
     }])
   };
