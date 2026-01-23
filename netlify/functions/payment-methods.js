861c5bb0b5036aed634b4ba82247654bb5b61c84
tedelston <tedelston@yahoo.ie>
2026-01-23 21:59:09 +0000

    Update payment-methods.js

--- netlify/functions/payment-methods.js
+++ netlify/functions/payment-methods.js
@@ -3,13 +3,13 @@ exports.handler = async function(event, context) {
     statusCode: 200,
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*", // Allow Snipcart to read this
+      "Access-Control-Allow-Origin": "*", 
     },
     body: JSON.stringify([{
-      id: "custom_stripe_checkout",
-      name: "Secure Checkout (Card, Apple Pay, Klarna)",
-      // IMPORTANT: We are hardcoding the full address to force a proper connection
-      checkoutUrl: "https://tedelston.netlify.app/.netlify/functions/create-session",
+      id: "custom_stripe_checkout_final",
+      name: "FINAL TEST (Click Me)",
+      // FIX: Use a relative path to stop domain redirects
+      checkoutUrl: "/.netlify/functions/create-session", 
       iconUrl: "https://upload.wikimedia.org/wikipedia/commons/b/ba/Stripe_Logo%2C_revised_2016.svg"
     }])
   };
